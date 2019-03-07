a9ccb9a08356ba1fc97bcd9f63a433d5dd808cf3
Alexandre Bonzom <bonzom.alexandre@gmail.com>
2019-03-07 09:42:05 +0100

    "correcting info in jso response"

--- routes/User.js
+++ routes/User.js
@@ -25,7 +25,11 @@ router.post("/sign_up", async (req, res) => {
       });
 
       await newUser.save();
-      return res.json(newUser.account, newUser.token, newUser._id);
+      return res.json({
+        account: newUser.account,
+        token: newUser.token,
+        id: newUser._id
+      });
     } else {
       return res.status(401).json({ message: "email already existing" });
     }
